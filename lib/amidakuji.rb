f268656c4080d80ca4897529deaa53a79e2aeeb1
shinokada <okada.shin@gmail.com>
2014-04-21 21:52:42 +0900

    Added #result but not completed

--- lib/amidakuji.rb
+++ lib/amidakuji.rb
@@ -6,29 +6,111 @@ class Amidakuji
   def initialize(column, rung)
     @column = column
     @rung = rung
-    @myarr = []
-    @per_arr = []
-    @build_arr = []
+    @accumulated_arr = []
+    #@per_arr = []
   end
 
   def permutate
     arr = (1..@column).to_a
-    @per_arr = arr.permutation(@column).to_a
+    arr.permutation(@column).to_a
   end
 
   def build_initial
-    @arr = (1..@column).to_a
+    @accumulated_arr[0] = (1..@column).to_a
   end
 
-  def swap_element 
+  def left_over(minuend, subtrahend)
+     minuend.delete  subtrahend
+     minuend
+  end
+
+  def swap_element
+    swapped_arr= []
     i = 0
     while i < @column - 1 do
       arr = build_initial
       new_a = swap(arr, i)
-      @build_arr << new_a
+      swapped_arr << new_a
       i += 1
     end
-    @build_arr
+    swapped_arr
+  end
+
+  def swap_item(arr)
+    (0...arr.size).each_cons(2).map do |i,j|
+      a = arr.dup
+      a[i], a[j] = a[j], a[i]
+      a
+    end
+  end
+
+  def subtract_arr
+    left_arr = []
+    permutate_arr = permutate
+    initial_arr = build_initial
+    # use left_over_arr for loop until there is nothing in the arr
+    left_over_arr = left_over(permutate_arr, initial_arr)
+    if left_over_arr.length > 0
+    # store first swapped element to accumulated_arr[1]
+    @accumulated_arr[1] = stage1 = swap_element
+    final = left_over(permutate_arr - stage1, initial_arr)
+    end
+    final
+  end
+
+  def result
+    left_over_arr = [] # possible array left
+    stage_arr =[] # where you collect array for each stage/rung
+    permutate_arr = permutate # all possible arrays
+    my_arr =[]
+    third_stage = []
+    left_over_arr1 = []
+    left_over_arr2 = []
+    # add initial array to stage_arr
+    # next gives [[1, 2, 3]]
+    stage_arr << build_initial
+    # use left_over_arr for loop until there is nothing in the arr
+    # first delete initial array [1, 2, 3]
+    # next gives [[1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
+    left_over_arr = left_over(permutate_arr, build_initial)
+    # find second stage array [[2,1,3],[1,3,2]]
+    second_stage = swap_item(build_initial)
+    # next gives lines 1 rung, [[1, 2, 3], [[2, 1, 3], [1, 3, 2]]]
+    stage_arr <<  second_stage
+    # next gives [[2, 3, 1], [3, 1, 2], [3, 2, 1]]
+    # delete second_stage from left_over_arr which gives [[2, 3, 1], [3, 1, 2], [3, 2, 1]]
+    left_over_arr1 = left_over_arr - second_stage
+    # find third stage
+    second_stage.each { |e| third_stage << swap_item(e)}
+    # next gives [[1, 2, 3], [2, 3, 1], [3, 1, 2]]
+    third_stage = third_stage.flatten(1).uniq
+    # add to stage_arr
+    stage_arr << third_stage
+    # next gives [[2, 3, 1], [3, 1, 2], [3, 2, 1]]
+    left_over_arr2 = left_over_arr1 - third_stage
+    left_over_arr2
+    stage_arr << left_over_arr2
+    stage_arr[2].count
+    # next gives [[1, 2, 3], [2, 3, 1], [3, 1, 2]]
+    # stage_arr.each{ |e| my_arr << swap_item(e)}
+   # my_arr.flatten(1).uniq
+
+    #while left_over_arr.length > 0 do
+   # # store first swapped element to accumulated_arr[1], etc
+   # # by using initial array and then swapped array
+   # # each of arr(initial or second arr) swap again to get next arr
+   # # find next swapped arr called stage
+    #  stage_arr.each{ |e| swap_item(e).each{ |i| my_arr  << i  }  }
+     # my_arr.uniq.each { |a| stage_arr << a  }
+     # left_over_arr = permutate_arr - stage_arr
+   # # add them to @accumulated_arr
+     # @accumulated_arr << stage_arr
+   # # delete them from left_over_arr
+      #left_over_arr = left_over(left_over_arr, stage_arr)
+   # # repeat it
+    #end
+    # outputs @accumulated_arr by array index 1: ...
+    #@accumlated_arr
   end
 
   def swap(arr, a)
@@ -39,7 +121,7 @@ class Amidakuji
   end
 
   def swaptwo(arr)
-    (0...arr.length - 1).map{|i| arr.dup.tap{ |a| a[i, 2] = a[i, 2].reverse } }
+    (0...arr.length - 1).map{ |i| arr.dup.tap{ |a| a[i, 2] = a[i, 2].reverse }}
   end
 
 end
--- spec/lib/amidakuji_spec.rb
+++ spec/lib/amidakuji_spec.rb
@@ -5,7 +5,12 @@ describe Amidakuji do
   context 'when vertical number is three' do
     before (:each) do
       @kuji1 = Amidakuji.new(3, 2)
+      @kuji2 = Amidakuji.new(4, 2)
       @arr1 = @kuji1.build_initial
+    end
+
+    it 'should display permutated array' do
+      expect(@kuji1.permutate).to eq ([[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]])
     end
 
     it 'should return [1, 2, 3]' do
@@ -16,17 +21,32 @@ describe Amidakuji do
       expect(@kuji1.permutate.count).to eq (6) 
     end
    
-    it 'should swap first and second element' do
+    it '#swap should swap first and second element[2, 1, 3]' do
       expect(@kuji1.swap(@arr1, 0)).to eq ([2, 1, 3])
     end
 
-    it 'should create an array with swapped elements' do
+    it '#swap_element should create an array with swapped elements [[2, 1, 3], [1, 3, 2]]' do
       expect(@kuji1.swap_element).to eq ([[2, 1, 3], [1, 3, 2]])
     end
 
-    it 'should swap two element of array and return array' do
+    it '#swaptwo should swap two element of array and return array as above' do
       expect(@kuji1.swaptwo(@arr1)).to eq([[2,1,3],[1,3,2]])
     end
-  #specify{ expect(@arr.swap(0, 1)).to eq([2, 1, 3, 4]) }
+
+    it '#subtract_arr should be an array permutate_arr without initial_arr and stage1/swap_element [[2, 3, 1], [3, 1, 2], [3, 2, 1]]' do
+      expect(@kuji1.subtract_arr).to eq ([[2, 3, 1], [3, 1, 2], [3, 2, 1]])
+    end
+
+    it '@kuji2 should count 20 array items after subtract_arr' do
+      expect(@kuji2.subtract_arr.count).to eq (20)
+    end
+
+    it '@kuji1#swap_item should be [[2,1,3],[1,3,2]]' do
+      expect(@kuji1.swap_item(@arr1)).to eq ([[2,1,3],[1,3,2]])
+    end
+    
+    it '#result should return all array' do
+      expect(@kuji1.result).to eq([[2,1,3],[1,3,2]])
+    end
   end
 end
